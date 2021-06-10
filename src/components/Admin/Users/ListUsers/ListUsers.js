3a1be2650fb3219ffbd131ecb901f7f6e8f5d2b2
IvanGTR43 <ivan_tr43@outlook.com>
2021-06-10 17:08:52 -0500

    asasa

---
+++ src/components/Admin/Users/ListUsers/ListUsers.js
@@ -0,0 +1,9 @@
+import React,{useState} from 'react';
+import { Switch, List, Avatar, Button } from "antd";
+import {  } from "@ant-design/icons";
+import NoAvatar from "../../../../assets/img/png/no-avatar.png";
+import "./ListUsers.scss";
+export default function ListUsers(props) {
+  const {usersActive, usersInactive} = props;
+  return "Lista de Usuarios";
+};
--- src/pages/Admin/Users/Users.js
+++ src/pages/Admin/Users/Users.js
@@ -1,18 +1,20 @@
 import React, {useState, useEffect} from 'react';
 import { getAccessTokenApi } from "../../../api/auth";
 import { getUsersApi } from "../../../api/user";
+import ListUsers from "../../../components/Admin/Users/ListUsers";
 import "./Users.scss";
 export default function Users(props) {
-  const [users, setUsers] = useState([]);
+  const [usersActive, setUsersActive] = useState([]);
+  const [usersInactive, setUsersInactive] = useState([]);
   const token = getAccessTokenApi();
   useEffect(() => {
     getUsersApi(token).then(response => {
-      setUsers(response);
+      setUsersActive(response);
     });
   }, [token]);
   return(
-    <div>
-      <h1>Lista de Usuarios</h1>
+    <div className="users">
+      <ListUsers/>
     </div>
   );
 };
